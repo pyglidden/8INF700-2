3d7c438948225b41ef1aed9527c6939f79837ace
Jeffrey Patrat <jeffrey.patrat1@uqac.ca>
2019-04-07 16:06:08 -0400

    PUSH

---
+++ NotSplinterCell/Assets/TimeChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeChanger : MonoBehaviour
+{
+    [SerializeField] float time, hero, objectS;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Coucou");
+        Time.timeScale = time;
+        MovingObject.globalSpeed = objectS;
+        CharacterController.globalSpeed = hero;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
