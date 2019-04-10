371396e042f13016335358a736c9ff661d0f7715
Jeffrey Patrat <jeffrey.patrat1@uqac.ca>
2019-04-10 10:01:21 -0400

    NewMovingObjectClass

--- NotSplinterCell/Assets/MovingObject.cs
+++ NotSplinterCell/Assets/MovingObject.cs
@@ -5,19 +5,69 @@ using UnityEngine;
 public class MovingObject : MonoBehaviour
 {
     public GameObject movingObject;
-    public Vector2 direction;
+    public Vector3 direction;
     public float speed;
-    public float waitingTime;
+    public enum axis { x, z };
+    public axis myAxe = axis.x;
     public static float globalSpeed;
+    public bool isPlatform = false;
+    public float initialCoordonates;
+    public float manoeuvreMarge=0;
+    bool isPositive = true;
+    float cumul;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movingObject = gameObject;
+
+
+        if (myAxe == axis.x)
+        {
+            initialCoordonates = gameObject.transform.position.x;
+            cumul = initialCoordonates;
+        }
+        else if (myAxe == axis.z)
+        {
+            initialCoordonates = gameObject.transform.position.z;
+            cumul = initialCoordonates;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(myAxe == axis.x)
+        {
+            if(isPositive){
+                movingObject.transform.Translate(new Vector3(manoeuvreMarge / 10f, 0, 0));
+                cumul += manoeuvreMarge;
+            }
+            else
+            {
+                movingObject.transform.Translate(new Vector3(-manoeuvreMarge / 10f, 0, 0));
+                cumul -= manoeuvreMarge;
+            }
+
+        }else if(myAxe == axis.z)
+        {
+            if (isPositive)
+            {
+                movingObject.transform.Translate(new Vector3(0, 0, manoeuvreMarge / 10f));
+                cumul += manoeuvreMarge;
+            }
+            else
+            {
+                movingObject.transform.Translate(new Vector3(0, 0, -manoeuvreMarge / 10f));
+                cumul -= manoeuvreMarge;
+            }
+        }
+
+        if(cumul == manoeuvreMarge)
+        {
+            isPositive = false;
+        }else if(cumul == -manoeuvreMarge)
+        {
+            isPositive = true;
+        }
     }
 }
